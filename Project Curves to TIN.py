0d943da7cdcf021a527611f5b72a66756fec72e9
Thomas Chapman <info@landarchtools.com>
2022-01-09 15:39:38 +1100

    Fixed bug when curve overhangs srf

--- Project Curves to TIN.py
+++ Project Curves to TIN.py
@@ -15,9 +15,8 @@ THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 # Project Curves to TIN
 
+from rhinoscript.object import FlashObject
 import rhinoscriptsyntax as rs
-
-# test change
 
 
 def main():
@@ -46,20 +45,38 @@ def main():
         rs.EnableObjectGrips(crv)
         grips = rs.ObjectGripLocations(crv)
         for grip in grips:
+
             zUp = rs.ShootRay(srf, grip, (0, 0, 1), 1)
-            if zUp != None:
+            # if zUp != None:
+            if zUp == None:
+                zUpList.append(False)
+            else:
                 zUpList.append(zUp[1])
 
             zDown = rs.ShootRay(srf, grip, (0, 0, -1), 1)
-
-            if zDown != None:
+            # if zDown != None:
+            if zDown == None:
+                zDownList.append(False)
+            else:
                 zDownList.append(zDown[1])
 
-        rs.CopyObject(crv)
-        if zUp != None:
-            rs.ObjectGripLocations(crv, zUpList)
-        else:
+        rs.CopyObject(crv)  # Coppy Existing curve
+
+        # Find the right list to iterate over and insert existing points for any falses
+        if all(x is False for x in zUpList):
+            falseindex = [i for i, val in enumerate(zDownList) if not val]
+            for i in falseindex:
+                # Replace False with existing grip location and closest Z value
+                closestPt = rs.BrepClosestPoint(srf, grips[i])
+                zDownList[i] = (grips[i].X, grips[i].Y, closestPt[0].Z)
             rs.ObjectGripLocations(crv, zDownList)
+        else:
+            falseindex = [i for i, val in enumerate(zUpList) if not val]
+            for i in falseindex:
+                # Replace False with existing grip location and closest Z value
+                closestPt = rs.BrepClosestPoint(srf, grips[i])
+                zUpList[i] = (grips[i].X, grips[i].Y, closestPt[0].Z)
+            rs.ObjectGripLocations(crv, zUpList)
 
         del zDownList[:]
         del zUpList[:]
