e15a003546ac7b7eafd16d93fcaa224087fa6505
Thomas Chapman <info@landarchtools.com>
2022-01-15 20:45:27 +1100

    Changes to Name

---
+++ MultiPatch.py
@@ -0,0 +1,34 @@
+'''
+
+Copyright <2021> <Thomas Chapman>
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'''
+
+# Patch Multiple Individual closed polylines
+# By Thomas Chapman 04/22/2021
+
+import rhinoscriptsyntax as rs
+
+
+def PatchMulti():
+
+    geo = rs.GetObjects('Select Closed Polylines', preselect=True)
+
+    if geo:
+
+        uv1 = rs.GetInteger('enter number of UV divisions',
+                            number=1, minimum=1)
+
+        rs.EnableRedraw(False)
+        for objects in geo:
+            rs.AddPatch((objects), (uv1, uv1), tolerance=0.0001)
+        rs.EnableRedraw(True)
+
+
+if __name__ == "__main__":
+    PatchMulti()
