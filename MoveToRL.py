bd547b84cac554f994587808cf823537a12a3f61
Thomas Chapman <info@landarchtools.com>
2022-06-12 15:18:13 +1000

    Added imperial Compat

--- MoveToRL.py
+++ MoveToRL.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 Copyright <2021> <Thomas Chapman>
 
@@ -7,62 +7,93 @@ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEM
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-'''
+"""
 
 # MOVE OBJECT TO SPECIFIED RL
-# BY THOMAS CHAPMAN ON 01/10/2021
+# BY THOMAS CHAPMAN ON 12/06/2022
 
 import rhinoscriptsyntax as rs
 import Rhino.Geometry as geo
+import Rhino as r
+import System
+import scriptcontext as sc
 
 
 def MoveToRL():
     try:
-        # Determine Unit system and scale m input to unit system scale and close if not mm, cm, m
-        def scale():
-            system = rs.UnitSystem()
-            if system == 2 or system == 3 or system == 4:
-                scaleFactorDict = {2: 1000, 3: 100, 4: 1}
-                scaleFactor = scaleFactorDict[system]
-                return scaleFactor
 
-            if system != 2 or system != 3 or system != 4:
-                return None
+        scale, imperial = scaling()
 
-        if scale() == None:
-            rs.MessageBox(
-                "This tool is can only be used in mm, cm or m model units")
-            return None
-
-        obj = rs.GetObjects('Select objects', preselect=True)
+        obj = rs.GetObjects("Select objects", preselect=True)
         if obj:
-            current = rs.GetPoint('Select point')
+            current = rs.GetPoint("Select point")
 
             if current:
-                rl = rs.GetString('RL to move to?')
+
+                if imperial == False:
+                    rl = rs.GetString("RL (m) to move to?")
+                if imperial == True:
+                    rl = rs.GetString("RL (ft) to move to?")
                 rl = float(rl)
-                rl = rl*scale()
+                rl = rl * scale
 
                 if rl == 0:  # move objects to the 0 coord
                     target3 = current.Z
                     if target3:
                         target3 = target3 * -1
-                        target4 = geo.Point3d(0, 0, target3)
+                        target4 = r.Geometry.Point3d(0, 0, target3)
                         rs.MoveObject(obj, target4)
 
                 elif rl < 0:
                     target5 = rl - current.Z
-                    target6 = geo.Point3d(0, 0, target5)
+                    target6 = r.Geometry.Point3d(0, 0, target5)
                     rs.MoveObject(obj, target6)
 
                 elif rl > 0:
                     target = rl - current.Z  # + or - number to target location
                     # translated vector needed to hit target
-                    target2 = geo.Point3d(0, 0, target)
+                    target2 = r.Geometry.Point3d(0, 0, target)
                     rs.MoveObject(obj, target2)
 
     except:
         print("Failed to execute")
+        rs.EnableRedraw(True)
+        return
+
+
+def scaling():
+    try:
+        unitNum = int(sc.doc.ModelUnitSystem)
+        # check to see if using metric
+
+        if (
+            unitNum != 1
+            and unitNum != 2
+            and unitNum != 3
+            and unitNum != 4
+            and unitNum != 5
+        ):
+            imperial = True
+            # struct unit system for current file
+            unitSystem = System.Enum.ToObject(r.UnitSystem, unitNum)
+            # struct unitsystem obj for script use, using feet (9)
+            internalSystem = System.Enum.ToObject(r.UnitSystem, 9)
+            # Scale units to model units
+            scale = r.RhinoMath.UnitScale(internalSystem, unitSystem)
+            if scale:
+                return scale, imperial
+        else:
+            imperial = False
+            # struct unit system for current file
+            unitSystem = System.Enum.ToObject(r.UnitSystem, unitNum)
+            # struct unitsystem obj for script use, using meteres (4)
+            internalSystem = System.Enum.ToObject(r.UnitSystem, 4)
+            # Scale units to model units
+            scale = r.RhinoMath.UnitScale(internalSystem, unitSystem)
+            if scale:
+                return scale, imperial
+    except:
+        print("Failed to find system scale")
         rs.EnableRedraw(True)
         return
 
