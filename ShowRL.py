2cbd44d5099e90e032a8b3a788399f7af2266cd9
Thomas Chapman <info@landarchtools.com>
2022-06-12 14:39:50 +1000

    Added imperial compat

--- ShowRL.py
+++ ShowRL.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 Copyright <2021> <Thomas Chapman>
 
@@ -7,40 +7,33 @@ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEM
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-'''
+"""
 
 
 # Show RL of specified point and copy to clipboard
 # By Thomas Chapman on 01/10/2021
 
 import rhinoscriptsyntax as rs
-import os
+import scriptcontext as sc
+import System
+import Rhino as r
 
 
 def ShowRL():
     try:
 
-        def scale():
-            system = rs.UnitSystem()
-            if system == 2 or system == 3 or system == 4:
-                scaleFactorDict = {2: 0.001, 3: 0.01, 4: 1}
-                scaleFactor = scaleFactorDict[system]
-                return scaleFactor
+        scale, imperial = scaling()
 
-            if system != 2 or system != 3 or system != 4:
-                return None
-
-        if scale() == None:
-            rs.MessageBox(
-                "This tool is can only be used in mm, cm or m model units")
-            return None
-
-        point = rs.GetPoint('Select point')
+        point = rs.GetPoint("Select point")
 
         if point:
             pointZ = point.Z
-        pointZ = pointZ*scale()
-        rs.AddTextDot('+RL ' + str(round(pointZ, 3)), point)
+        pointZ = pointZ * scale
+
+        if imperial == False:
+            rs.AddTextDot("+RL " + str(round(pointZ, 3)) + " m", point)
+        if imperial == True:
+            rs.AddTextDot("+RL " + str(round(pointZ, 3)) + " ft", point)
 
         # Copy RL to Clipboard
         RL = str(round(pointZ, 3))
@@ -48,6 +41,43 @@ def ShowRL():
 
     except:
         print("Failed to execute")
+        rs.EnableRedraw(True)
+        return
+
+
+def scaling():
+    try:
+        unitNum = int(sc.doc.ModelUnitSystem)
+        # check to see if using metric
+
+        if (
+            unitNum != 1
+            and unitNum != 2
+            and unitNum != 3
+            and unitNum != 4
+            and unitNum != 5
+        ):
+            imperial = True
+            # struct unit system for current file
+            unitSystem = System.Enum.ToObject(r.UnitSystem, unitNum)
+            # struct unitsystem obj for script use, using feet (9)
+            internalSystem = System.Enum.ToObject(r.UnitSystem, 9)
+            # Scale units to model units
+            scale = r.RhinoMath.UnitScale(unitSystem, internalSystem)
+            if scale:
+                return scale, imperial
+        else:
+            imperial = False
+            # struct unit system for current file
+            unitSystem = System.Enum.ToObject(r.UnitSystem, unitNum)
+            # struct unitsystem obj for script use, using meteres (4)
+            internalSystem = System.Enum.ToObject(r.UnitSystem, 4)
+            # Scale units to model units
+            scale = r.RhinoMath.UnitScale(unitSystem, internalSystem)
+            if scale:
+                return scale, imperial
+    except:
+        print("Failed to find system scale")
         rs.EnableRedraw(True)
         return
 
